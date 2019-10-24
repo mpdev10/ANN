c11329c006ed29d2b265e3aa249f614721a6d5f4
mateuszp <mpftp10@gmail.com>
2019-10-24 23:02:48 +0200

    Fixed relu activation

--- ActivationImpl.py
+++ ActivationImpl.py
@@ -7,4 +7,4 @@ class ActivationImpl:
     sigmoid = ActivationFunction.of(lambda x: 1.0 / (1.0 + np.exp(-x)),
                                     lambda x: (1.0 / (1.0 + np.exp(-x))) * (1 - (1.0 / (1.0 + np.exp(-x)))))
 
-    relu = ActivationFunction.of(lambda x: np.max(0, x), lambda x: np.max(0, np.min(1, x)))
+    relu = ActivationFunction.of(lambda x: np.maximum(0, x), lambda x: np.maximum(0, np.minimum(1, x)))
